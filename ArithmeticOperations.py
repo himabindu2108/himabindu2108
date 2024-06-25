305a12956de91d88b747e3bb2ee91ee71d9da9b8
himabindu2108 <binduyallapragada.20@gmail.com>
2024-06-25 06:17:09 +0530

    Add files via upload

---
+++ ArithmeticOperations.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ed389f0e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "FirstNumber = int(input(\"Enter first number:\"))\n",
+    "SecondNumber = int(input(\"Enter second number:\"))\n",
+    "print(\"addition of two given numbers:\", FirstNumber + SecondNumber)\n",
+    "print(\"Subtraction of two given numbers:\", FirstNumber - SecondNumber)\n",
+    "print(\"Multiplication of two given numbers:\", FirstNumber * SecondNumber)\n",
+    "print(\"Division of two given numbers:\", FirstNumber/SecondNumber)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
